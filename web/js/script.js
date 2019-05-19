d6c0a1f30fb560b53d584643958a4c58e5837d64
NataliaPavlova87 <natalia_pavlova@bk.ru>
2019-05-19 20:43:01 +0300

    Update script.js

--- web/js/script.js
+++ web/js/script.js
@@ -1,15 +1,22 @@
-$.getJSON('https://api.myjson.com/bins/gufba', function(data){
-    $.each(data, function(key, val){
-      for(let i = 0; i < val.length; i++) {
-          $('#list').append(`<a href="${val[i].id}">${val[i].name}</a></br>`)
-      }
+$(document).ready(function(){
+  if($('div').is('#list')) {
+    $.getJSON('https://api.myjson.com/bins/pzvni', function(data){
+      $.each(data, function(key, val){
+        for(let i = 0; i < val.length; i++) {
+            $('#list').append(`<a href="${val[i].url}" data-id="${val[i].id}">${val[i].name}</a></br>`)
+        }
+      });
     });
-  });
+  }
 
-$.getJSON('https://api.myjson.com/bins/c5rjy', function(data){
-    $.each(data, function(key, val){
-      for(let i = 0; i < val.length; i++) {
-          $('#types').append(`<a href="${val[i].id}">${val[i].name}</a></br>`)
-      }
+  if($('div').is('#types')) {
+    $.getJSON('https://api.myjson.com/bins/jzefy', function(data){
+      $.each(data, function(key, val){
+        for(let i = 0; i < val.length; i++) {
+            $('#types').append(`<a href="${val[i].url}" data-id="${val[i].id}">${val[i].name}</a></br>`)
+        }
+      });
     });
-  });
+  }
+
+});
