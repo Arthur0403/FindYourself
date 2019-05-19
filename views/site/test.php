c1445d310f009326796d23c1cbbdc3f726e6256e
NataliaPavlova87 <natalia_pavlova@bk.ru>
2019-05-19 22:52:01 +0300

    Update test.php

--- views/site/test.php
+++ views/site/test.php
@@ -7,6 +7,10 @@
 
     <p><a class="btn btn-lg btn-success" href="/site/list">Назад</a></p>
 
+    <h2 class="test-name"></h2>
+    
+    <div id="test"></div>
+
     <p><a class="btn btn-lg btn-success" href="/site/..">Результаты</a></p>
 
 </div>
