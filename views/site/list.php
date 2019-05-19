82b3a5cbbc712172d311a29584d26d1feaaf3809
NataliaPavlova87 <natalia_pavlova@bk.ru>
2019-05-19 22:52:05 +0300

    Update list.php

--- views/site/list.php
+++ views/site/list.php
@@ -6,9 +6,10 @@
 <div class="site-list">
 
     <p><a class="btn btn-lg btn-success" href="/site/types">Назад</a></p>
-
+        
     <h2>Список тестов</h2>
 
     <div id="list"></div>
+
 
 </div>
