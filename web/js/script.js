116a443f8f091bd1b9078b5b39c69c8c63118fc9
NataliaPavlova87 <natalia_pavlova@bk.ru>
2019-05-19 22:51:55 +0300

    Update script.js

--- web/js/script.js
+++ web/js/script.js
@@ -19,4 +19,19 @@ $(document).ready(function(){
     });
   }
 
+  if($('div').is('#test')) {
+    $.getJSON('https://api.myjson.com/bins/123eny', function(data){
+        $('.test-name').text(`${data.name}`);
+          for(let i = 0; i < data.questions.length; i++) {
+            if(typeof(data.questions[i]) === 'string') {
+              $('#test').append(`<p>${data.questions[i]}</p>`);
+            } else {
+              for(let j = 0; j < data.questions[i].length; j++) {
+                $('#test').append(`<input type="checkbox" class="radio" id="${data.questions[i][j].id}"><label for="${data.questions[i][j].id}">${data.questions[i][j].answer}</label>`);
+              }
+          }
+        }
+    });
+  }
+
 });
