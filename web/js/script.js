c0f2669cedbdf92c46b1a593b15a775c511889a6
NataliaPavlova87 <natalia_pavlova@bk.ru>
2019-05-21 23:31:15 +0300

    Update script.js

--- web/js/script.js
+++ web/js/script.js
@@ -34,4 +34,15 @@ $(document).ready(function(){
     });
   }
 
+
+  $('.owl-carousel').owlCarousel({
+    loop: false,
+    margin: 10,
+    nav: false,
+    items: 1,
+    dots: true,
 });
+
+});
+
+
