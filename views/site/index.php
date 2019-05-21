3fd191ad1e33b4fc13602911a4928b299dbd5ffa
NataliaPavlova87 <natalia_pavlova@bk.ru>
2019-05-21 23:30:59 +0300

    Update index.php

--- views/site/index.php
+++ views/site/index.php
@@ -5,12 +5,13 @@
 $this->title = 'My Yii Application';
 ?>
 <div class="site-index">
-
-    <div class="jumbotron">
+    <img src="../img/findyourself.png" alt="">
+    <a href="/site/banner" class="index-link">Найди <span>себя</span></a>
+    <!-- <div class="jumbotron">
         <h1>Добро пожаловать!</h1>
 
         <p><a class="btn btn-lg btn-success" href="/site/types">Тестирование</a></p>
         <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Список профессий</a></p>
         <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Список курсов</a></p>
-    </div>
+    </div> -->
 </div>
