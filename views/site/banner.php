8c88c63f268c6ce2d1de25f263a232f01ca4cab4
NataliaPavlova87 <natalia_pavlova@bk.ru>
2019-05-21 23:30:57 +0300

    Create banner.php

---
+++ views/site/banner.php
@@ -0,0 +1,27 @@
+<?php
+
+/* @var $this yii\web\View */
+
+?>
+<div class="site-banner">
+<div class="owl-carousel owl-theme">
+    <div class="item">
+        <img src="../img/findyourself.png" alt="">
+        <p>Проходи тесты</p>      
+    </div>
+    <div class="item">
+        <img src="../img/findyourself.png" alt="">
+        <p>Выбирай профессию</p>
+    </div>
+    <div class="item">
+        <img src="../img/findyourself.png" alt="">
+        <p>Дополняй свое резюме</p>
+    </div>
+    <div class="item">
+        <img src="../img/findyourself.png" alt="">
+        <p>Получай качественные результаты</p>
+    </div>
+</div>
+<a href="/site/start" class="banner-link">Начать сейчас</a>
+
+</div>
