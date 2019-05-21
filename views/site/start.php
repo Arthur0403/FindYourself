abb38a5fb5fcf7b407670fc476a597f438966da1
NataliaPavlova87 <natalia_pavlova@bk.ru>
2019-05-21 23:31:01 +0300

    Create start.php

---
+++ views/site/start.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $this yii\web\View */
+
+?>
+<div class="site-start">
+    <div class="jumbotron">
+        <p><a class="btn btn-lg btn-success" href="/site/types">Тестирование</a></p>
+        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Список профессий</a></p>
+        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Список курсов</a></p>
+    </div>
+</div>
