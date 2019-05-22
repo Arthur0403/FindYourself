3d40324f1308cbdcd0cfff33774a35e93904a09a
NataliaPavlova87 <natalia_pavlova@bk.ru>
2019-05-22 23:12:38 +0300

    Update start.php

--- views/site/start.php
+++ views/site/start.php
@@ -4,9 +4,11 @@
 
 ?>
 <div class="site-start">
+    <div class="grey-line"></div>
+    <div class="dark-line"></div>
     <div class="jumbotron">
-        <p><a class="btn btn-lg btn-success" href="/site/types">Тестирование</a></p>
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Список профессий</a></p>
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Список курсов</a></p>
+        <p><a class="btn btn-lg" href="/site/types">Тестирование</a></p>
+        <p><a class="btn btn-lg" href="http://www.yiiframework.com">Список профессий</a></p>
+        <p><a class="btn btn-lg" href="http://www.yiiframework.com">Список курсов</a></p>
     </div>
 </div>
